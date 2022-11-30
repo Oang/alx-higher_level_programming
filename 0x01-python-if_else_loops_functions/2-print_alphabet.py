5728440dcf61bbeb4c8faab6069b0260a528225b
Olang Philemon <junanaphil@gmail.com>
2022-12-01 01:20:34 +0300

    Update 2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-for char in range(26):
-    print("{:s}".format(chr(char + ord("a"))), end="")
+number = 97
+while number < 123:
+    print("{:s}".format(chr(number)), end='')
+    number += 1
